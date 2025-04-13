31eec23eeb09095ca713db78b9af51d17afe1aa0
kondyan <arsenij.kondratenko@gmail.com>
2025-04-13 21:28:51 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -939,17 +939,50 @@
 
 // const titleRef = document.createElement("h2");
 
-let result = [];
-for (i of nums) {
-  const x = target - i;
-  if (
-    nums.includes(x, nums.indexOf(i) + 1) &&
-    nums.indexOf(x, nums.indexOf(i) + 1) !== nums.indexOf(i)
-  ) {
-    const iIndex = nums.indexOf(i);
-    const xIndex = nums.indexOf(x, iIndex + 1);
-    result.push(iIndex, xIndex);
-    break;
+// let result = [];
+// for (i of nums) {
+//   const x = target - i;
+//   if (
+//     nums.includes(x, nums.indexOf(i) + 1) &&
+//     nums.indexOf(x, nums.indexOf(i) + 1) !== nums.indexOf(i)
+//   ) {
+//     const iIndex = nums.indexOf(i);
+//     const xIndex = nums.indexOf(x, iIndex + 1);
+//     result.push(iIndex, xIndex);
+//     break;
+//   }
+// }
+// return result;
+
+{
+  const button = document.querySelector(".user-tap-button");
+  let reachedZeroEnergy = false;
+
+  function tick() {
+    try {
+      const energy = document.querySelector(".user-tap-energy > p");
+      if (energy) {
+        const energyStr = energy.innerText;
+        const currEnergy = Number(energyStr.split("/")[0]);
+        const MaxEnergy = Number(energyStr.split("/")[1]);
+
+        if (!reachedZeroEnergy) {
+          for (let i = 0; i < 30; i++) {
+            button.dispatchEvent(new PointerEvent("pointerup"));
+          }
+        }
+        if (currEnergy <= 10) {
+          reachedZeroEnergy = true;
+        }
+        if (currEnergy >= MaxEnergy - 10) {
+          reachedZeroEnergy = false;
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    setTimeout(tick, 100 * Math.random() + 50);
   }
+
+  tick();
 }
-return result;
